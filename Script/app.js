3e5def36e02b9e228ff22deee497479f8c752222
jean.depaula <jean.depaula@obraprimaweb.com.br>
2021-11-20 07:22:02 -0300

    First Commit - Mobile

---
+++ Script/app.js
@@ -0,0 +1,76 @@
+/*Abrir e fechar menu*/
+
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for(const element of toggle){
+    element.addEventListener('click', function(){
+        nav.classList.toggle('show')
+    })
+}
+
+/*Esconder menu ao clicar nos itens*/
+
+const links = document.querySelectorAll('nav ul li a')
+
+for(const item of links){
+    item.addEventListener('click', function(){
+        nav.classList.remove('show')
+    })
+}
+
+/* Sombra no menu com scroll */
+function MenuShadowScroll(){
+    const header = document.querySelector('#header')
+    const navHeight = header.offsetHeight
+
+    if(window.scrollY >= navHeight){
+        header.classList.add('scroll')
+    }else{
+        header.classList.remove('scroll')
+    }
+}
+
+/* Slider */
+const swiper = new Swiper('.swiper', {
+    pagination: {
+        el: '.swiper-pagination',
+      },
+    slidesPerView: 1,
+    mousewheel: true,
+    keyboard: true
+})
+
+/* Animação Scroll */
+const scrollReveal = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+    reset: true
+})
+
+scrollReveal.reveal(`
+    #home .image, #home .text,
+    #about .image, #about .text,
+    #services header, #services .card,
+    #testimonials header, #testimonials .testimonials,
+    #contact .text, #contact .links,
+    footer .brand, footer .social
+`,{interval: 100})
+
+/* Botão voltar ao topo */
+
+function BackToTop(){
+    const buttonBackToTop = document.querySelector(".back-to-top")
+
+    if(window.scrollY>=560){
+        buttonBackToTop.classList.add('show')
+    } else{
+        buttonBackToTop.classList.remove('show')
+    }
+}
+
+window.addEventListener('scroll',function(){
+    BackToTop()
+    MenuShadowScroll()
+})
